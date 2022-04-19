ab46ae94849b4d56cdbe52b2ab9026dac2cc15e7
Dinz <kostan-82@yandex.ru>
2022-04-19 23:20:24 +0300

    4. Многопоточность в базе данных [#504860]

---
+++ src/main/java/ru/job4j/dreamjob/control/RegistrationController.java
@@ -0,0 +1,43 @@
+package ru.job4j.dreamjob.control;
+
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.job4j.dreamjob.model.User;
+import ru.job4j.dreamjob.service.UserService;
+
+import java.util.Optional;
+
+@ThreadSafe
+@Controller
+public class RegistrationController {
+
+    private final UserService userService;
+
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/registrationUser")
+    public String registration(Model model, @ModelAttribute User user) {
+        Optional<User> regUser = userService.add(user);
+        if (regUser.isEmpty()) {
+            model.addAttribute("message", "Пользователь с такой почтой уже существует");
+            return "redirect:/fail";
+        }
+        return "redirect:/success";
+    }
+
+    @GetMapping("/registrationUser")
+    public String registrationUser(Model model) {
+        return "registrationUser";
+    }
+
+    @GetMapping("/fail")
+    public String fail(Model model) {
+        return "fail";
+    }
+}
---
+++ src/main/java/ru/job4j/dreamjob/store/UserDbStore.java
@@ -0,0 +1,41 @@
+package ru.job4j.dreamjob.store;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.stereotype.Repository;
+import ru.job4j.dreamjob.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Optional;
+
+@Repository
+public class UserDbStore {
+
+    private final BasicDataSource pool;
+
+    public UserDbStore(BasicDataSource pool) {
+        this.pool = pool;
+    }
+
+    public Optional<User> add(User user) {
+        try (Connection connection = pool.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "INSERT INTO users(email, password) VALUES (?, ?)",
+                     PreparedStatement.RETURN_GENERATED_KEYS)
+        ) {
+            preparedStatement.setString(1, user.getEmail());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.execute();
+            try (ResultSet id = preparedStatement.getGeneratedKeys()) {
+                if (id.next()) {
+                    user.setId(id.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+        return Optional.of(user);
+    }
+}
